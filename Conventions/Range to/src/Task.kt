8a0d96d8f363923f7317f55b82cce0af6b5be820
mohamedhatemabdu <mohamedhatemabdu@gmail.com>
2019-10-14 22:14:06 +0200

    rangeTo + InRange+ ForLoop

--- Conventions/Range to/src/Task.kt
+++ Conventions/Range to/src/Task.kt
@@ -1,6 +1,8 @@
-operator fun MyDate.rangeTo(other: MyDate) = TODO()
 
-class DateRange(override val start: MyDate, override val endInclusive: MyDate): ClosedRange<MyDate>
+// TODO : to more understand the problem of overiding the range TO
+operator fun MyDate.rangeTo(other: MyDate) = DateRange(this, other)
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>
 
 fun checkInRange(date: MyDate, first: MyDate, last: MyDate): Boolean {
     return date in first..last
