df99790f163bbd576e294af0ed9709cfdefa4410
mohamedhatemabdu <mohamedhatemabdu@gmail.com>
2019-10-16 07:40:46 +0200

    flatmap task

--- Collections/FlatMap/src/Task.kt
+++ Collections/FlatMap/src/Task.kt
@@ -1,5 +1,8 @@
 // Return all products this customer has ordered
-fun Customer.getOrderedProducts(): Set<Product> = TODO()
+//TODO : Digest more into the diff between map and flatmap
+fun Customer.getOrderedProducts(): Set<Product> = orders.flatMap { it.products }.toSet()
 
 // Return all products that were ordered by at least one customer
-fun Shop.getAllOrderedProducts(): Set<Product> = TODO()
+//fun Shop.getAllOrderedProducts(): Set<Product> = customers.flatMap { it.orders }.flatMap { it.products }.toSet()
+fun Shop.getAllOrderedProducts(): Set<Product> = customers.flatMap { it.getOrderedProducts() }.toSet()//fun Shop.getAllOrderedProducts(): Set<Product> = customers.flatMap { it.orders }.flatMap { it.products }.toSet()
+
